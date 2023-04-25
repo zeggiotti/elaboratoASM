86eba0c12951c396a8a0514d527b83edc8df71d2
zeggiotti <ettore.zeggiotti@gmail.com>
2023-04-25 11:50:54 +0200

    trovati i valori nello stack

--- src/usermode.s
+++ src/usermode.s
@@ -6,7 +6,8 @@
 .type usermode, @function
 
 usermode:
-    movl -8(%esp),%ecx
+    movl 4(%esp), %eax          # Carica il numero di parametri del programma. Non si usa pop perche in cima c'è il PC e non va tolto.
+    movl 12(%esp), %ebx         # Carica l'inirizzo della stringa passata come parametro. Non è a 8(%esp) perche li ce l'indirizzo del nome del programma.
 
     cmp %ecx,%edx
 
